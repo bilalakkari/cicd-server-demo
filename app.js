b89a0fd81022e441a08653cc5559c3ebfcc32b39
Bilal <bilal.akkari101@gmail.com>
2026-06-12 06:13:09 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hi');
+})
+
+app.listen(PORT);
